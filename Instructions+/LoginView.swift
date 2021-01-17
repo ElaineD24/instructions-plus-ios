ebdd7dc39f038b01359f7a0ee33544b0a8a188af
Elaine Deng <deviny-deng@outlook.com>
2021-01-16 19:57:05 -0500

    logins

--- Instructions+/LoginView.swift
+++ Instructions+/LoginView.swift
@@ -7,14 +7,139 @@
 
 import SwiftUI
 
-struct LoginView: View {
+let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 1.0)
+
+let storedUsername = "1234"
+let storedPassword = "1234"
+
+struct LoginView : View {
+    
+    @State var username: String = ""
+    @State var password: String = ""
+    
+    @State var authenticationDidFail: Bool = false
+    @State var authenticationDidSucceed: Bool = false
+    
+    @State var editingMode: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            VStack {
+                WelcomeText()
+//                UserImage()
+                UsernameTextField(username: $username, editingMode: $editingMode)
+                PasswordSecureField(password: $password)
+                if authenticationDidFail {
+                    Text("Information not correct. Try again.")
+                        .offset(y: -10)
+                        .foregroundColor(.red)
+                }
+                Button(action: {
+                    if self.username == storedUsername && self.password == storedPassword {
+                        self.authenticationDidSucceed = true
+                        self.authenticationDidFail = false
+                        self.hideKeyboard()
+                    } else {
+                        self.authenticationDidFail = true
+                        self.hideKeyboard()
+                    }
+                }) {
+                    LoginButtonContent()
+                }
+                }
+                .padding()
+            if authenticationDidSucceed {
+              
+                ContentView()
+            }
+        }
+            .offset(y: editingMode ? -150 : 0)
     }
 }
 
-struct LoginView_Previews: PreviewProvider {
+#if DEBUG
+struct LoginView_Previews : PreviewProvider {
     static var previews: some View {
         LoginView()
     }
 }
+#endif
+
+struct WelcomeText : View {
+    var body: some View {
+        return Text("Welcome!")
+            .font(.largeTitle)
+            .fontWeight(.semibold)
+            .padding(.bottom, 20)
+    }
+}
+
+//struct UserImage : View {
+//    var body: some View {
+//        return Image("userImage")
+//            .resizable()
+//            .aspectRatio(contentMode: .fill)
+//            .frame(width: 150, height: 150)
+//            .clipped()
+//            .cornerRadius(150)
+//            .padding(.bottom, 75)
+//    }
+//}
+
+struct LoginButtonContent : View {
+    var body: some View {
+        return Text("LOGIN")
+            .font(.headline)
+            //change
+            .foregroundColor(.white)
+            .padding()
+            .frame(width: 220, height: 60)
+            .background(Color.blue)
+            .cornerRadius(15.0)
+    }
+}
+
+struct UsernameTextField : View {
+    
+    @Binding var username: String
+    
+    @Binding var editingMode: Bool
+    
+    var body: some View {
+        return TextField("Username", text: $username, onEditingChanged: {edit in
+            if edit == true
+            {self.editingMode = true}
+            else
+            {self.editingMode = false}
+        })
+            .padding()
+            .background(lightGreyColor)
+            .cornerRadius(5.0)
+            .padding(.bottom, 20)
+    }
+}
+
+struct PasswordSecureField : View {
+    
+    @Binding var password: String
+    
+    var body: some View {
+        return SecureField("Password", text: $password)
+            .padding()
+            .background(lightGreyColor)
+            .cornerRadius(5.0)
+            .padding(.bottom, 20)
+    }
+}
+
+
+#if canImport(UIKit)
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+#endif
+
+
--- Instructions+/SocialLoginView.swift
+++ Instructions+/SocialLoginView.swift
@@ -1,20 +1,38 @@
 //
-//  SocialLoginView.swift
-//  Instructions+
-//
-//  Created by Wenyue Deng on 2021-01-16.
+//  ContentView.swift
+//  signin
 //
 
+// add your google plist before running it to avoid crashes
+// also add reserved client id
+
 import SwiftUI
+import GoogleSignIn
 
 struct SocialLoginView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        google().frame(width: 120, height: 50)
     }
 }
 
 struct SocialLoginView_Previews: PreviewProvider {
     static var previews: some View {
         SocialLoginView()
+    }
+}
+
+struct google : UIViewRepresentable {
+    
+
+    func makeUIView(context: UIViewRepresentableContext<google>) -> GIDSignInButton {
+        
+        let button = GIDSignInButton()
+        button.colorScheme = .dark
+        GIDSignIn.sharedInstance()?.presentingViewController = UIApplication.shared.windows.last?.rootViewController
+        return button
+    }
+    func updateUIView(_ uiView: GIDSignInButton, context: UIViewRepresentableContext<google>) {
+        
     }
 }

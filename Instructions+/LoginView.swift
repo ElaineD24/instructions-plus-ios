fcb433dff62735486dd99f364bef9d66530ddf7f
Elaine Deng <deviny-deng@outlook.com>
2021-01-16 19:53:46 -0500

    email login UI

---
+++ Instructions+/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  Instructions+
+//
+//  Created by Wenyue Deng on 2021-01-16.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
---
+++ Instructions+/SocialLoginView.swift
@@ -0,0 +1,20 @@
+//
+//  SocialLoginView.swift
+//  Instructions+
+//
+//  Created by Wenyue Deng on 2021-01-16.
+//
+
+import SwiftUI
+
+struct SocialLoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SocialLoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        SocialLoginView()
+    }
+}
